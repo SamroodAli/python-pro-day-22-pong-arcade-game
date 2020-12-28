90caf81f086967391740545026767eaabaf369a0
SamroodAli <samroodali@live.com>
2020-12-28 11:02:29 +0530

    Fix:paddle up and down setheading

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from screen import screen, width, height
 from paddles import Paddle
 # paddle instance
-left_paddle = Paddle(position=(-380, 0))
-right_paddle = Paddle(position=(380, 0))
+left_paddle = Paddle(position=(-380, 0), keys=("e", "d"))
+right_paddle = Paddle(position=(380, 0), keys=("Up", "Down"))
 screen.update()
 screen.exitonclick()
--- paddles.py
+++ paddles.py
@@ -1,10 +1,27 @@
-from turtle import Turtle
+from turtle import Turtle , onkey
+from screen import screen
+
 
 class Paddle(Turtle):
-    def __init__(self, position):
+    def __init__(self, position, keys):
         super().__init__()
         self.shape("square")
         self.penup()
         self.color("white")
         self.goto(position)
-        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.turtlesize(stretch_len=1, stretch_wid=5)
+        self.event_listeners(keys)
+
+    def paddle_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+        screen.update()
+
+    def paddle_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+        screen.update()
+
+    def event_listeners(self, keys):
+        onkey(key=keys[0], fun=self.paddle_up)
+        onkey(key=keys[1], fun=self.paddle_down)
