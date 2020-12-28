e8da1e225582158e3df18c35cfed5f63dbf6dbd7
SamroodAli <samroodali@live.com>
2020-12-28 15:07:00 +0530

    Fix:ball movement algorithm changed

--- ball.py
+++ ball.py
@@ -1,6 +1,6 @@
 from turtle import Turtle
-import random
-
+from random import choice
+STARTING_DIRECTIONS = [10, -10]
 
 class Ball(Turtle):
     def __init__(self):
@@ -8,8 +8,15 @@ class Ball(Turtle):
         self.color("white")
         self.shape("circle")
         self.penup()
+        # Choice is from random module
+        self.x_move = choice(STARTING_DIRECTIONS)
+        self.y_move = choice(STARTING_DIRECTIONS)
 
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
+    def bounce(self):
+        self.y_move *= -1
+
--- main.py
+++ main.py
@@ -2,15 +2,21 @@ from screen import screen, width, height
 from paddles import Paddle
 from ball import Ball
 import time
+import random
 # paddle instance
 left_paddle = Paddle(position=(-380, 0), keys=("e", "d"))
 right_paddle = Paddle(position=(380, 0), keys=("Up", "Down"))
 # ball instance
 game_is_on = True
 ball = Ball()
+starting_position = random.randint(0, 359)
+ball.setheading(starting_position)
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     ball.move()
-
+    # collision with wall
+    if ball.ycor() > 275 or ball.ycor() < -275:
+        ball.bounce()
+    # Collision with paddle
 screen.exitonclick()
